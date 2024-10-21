6ccd3c70d17ba980b957e7a6e67b3293c98ee23c
Tanmay Maity <tanmaymaity06@gmail.com>
2024-10-21 14:25:33 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +14,4 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
+reportWebVitals();
